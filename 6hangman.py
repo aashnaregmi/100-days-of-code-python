c9dc3f85cf9f85e7372cc09da6fa4ab3e2749984
aashnaregmi <regmiashna25@gmail.com>
2026-06-24 15:51:02 +0545

    refactor(day-07): improve Hangman with input validation, repeated guess handling, and corrected life logic

--- 6hangman.py
+++ 6hangman.py
@@ -62,27 +62,33 @@ totallen=len(chosen_word)
 placeholder = ["_"] * totallen
 
 lifeline = 6
+guessedword=[]
 
 
 
 
 
-while  lifeline>=0 and "_" in placeholder:
+while  lifeline >0 and "_" in placeholder:
     placeholderstr="".join(placeholder)
     print(f"Guess word :{placeholderstr}")
     guess = input("Guess a letter : ").lower()
-    if guess in placeholder:
+    if guess in guessedword:
         print(f"You already guessed :{guess}.\nGuess another word ")
-    elif guess in chosen_word:
-       for i in range(0,totallen):
+    else:
+        guessedword.append(guess)
+        if len(guess) != 1 or not guess.isalpha():
+           print("Please enter only one alphabet.")
+           continue
+        if guess in chosen_word:
+         for i in range(0,totallen):
            if chosen_word[i]==guess:
                placeholder[i]=guess
 
-    else:
-        print(f"\'{guess}\' is not there ")
-        print(f"{stages[lifeline]}")
-        lifeline-=1
-        print(f"lifeline:{lifeline}/6")
+        else:
+            print(f"\'{guess}\' is not there ")
+            lifeline-=1
+            print(f"{stages[lifeline]}")
+            print(f"lifeline:{lifeline}/6")
 
 
 if "_" not in placeholder:
